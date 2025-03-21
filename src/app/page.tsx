8e2601036493c873aac8a8c88f7e775f090e80ca
ItsPi3141 <90981829+ItsPi3141@users.noreply.github.com>
2025-03-21 12:34:15 -0700

    feat: add threejs canvas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+"use client";
+import { Scroll, ScrollControls, Text } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
 export default function Home() {
-	return <p>Hello!</p>;
+	return (
+		<div className="bg-black w-screen h-screen">
+			<Canvas>
+				<ScrollControls
+					pages={3}
+					damping={0.25}
+					style={{
+						scrollbarWidth: "none",
+					}}
+				>
+					<Scroll>
+						<Text>ItsPi3141</Text>
+					</Scroll>
+				</ScrollControls>
+			</Canvas>
+		</div>
+	);
 }
